4b7d3d923d00e67f05c13bb341288a51e89eabc6
Björn Hallström <bjorn.hallstrom@skane.se>
2019-09-23 12:24:17 +0200

    Add all container stuff from nexomeflow. Also add older version of sentieon

---
+++ bin/exome_DPAF_filter.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+use strict;
+
+if( $ARGV[0] =~ /\.gz$/ ) {
+    open( VCF, "zcat $ARGV[0]|" );
+}
+else {
+    open( VCF, "$ARGV[0]" );
+}
+
+while( <VCF> ) {
+    if( /^#/ ) {
+	print;
+    }
+    else {
+	chomp;
+
+	my @a = split /\t/;
+	my $keep = 1;
+	for(9..$#a) {
+	    my @a = split /:/, $a[$_];
+	    
+	    my @allele_counts = split /,/, $a[1];
+	    my $dp = $a[2];
+
+	    if( $dp eq "." or $dp == 0 ) {
+		$keep = 0;
+		last;
+	    }
+	    elsif( $a[0] ne "0/0" and $allele_counts[1] < 3 ) {
+		$keep = 0;
+		last;
+	    }
+	    elsif( $a[0] ne "0/0" and $allele_counts[1]/$dp < 0.3 ) {
+		$keep = 0;
+		last;
+	    }
+	}
+	print $_."\n" if $keep;
+    }
+    
+}
+
+close VCF;
