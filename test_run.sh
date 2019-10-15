1a4879e06162bd94501338437618823ecfc6447a
Viktor Henmyr <viktor_henmyr@hotmail.com>
2019-10-15 16:08:54 +0200

    added flags

--- test_run.sh
+++ test_run.sh
@@ -1,6 +1,8 @@
 nextflow run main.nf \
-	 --csv /fs1/proj/test_real_trio.csv \
-	 -with-singularity /fs1/resources/containers/wgs_active.sif \
-	 -resume
+	--csv /fs1/proj/test_real_trio.csv \
+	-with-singularity /fs1/resources/containers/wgs_active.sif \
+	--align
+	--annotate
+	-resume
 
  
