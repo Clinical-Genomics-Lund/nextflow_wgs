ce0b0066eb8850a3057b0aadcdcfe441c1b281ae
Björn Hallström <bjorn.hallstrom@gmail.com>
2020-04-29 08:24:39 +0200

    Fix incorrect order in header of UPDtable

--- bin/upd_table.pl
+++ bin/upd_table.pl
@@ -13,7 +13,7 @@ while(<IN>) {
 
 my @chroms = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X);
 
-print "Chromosome\tNon-informative\tTotal SNPs\tMismatch father\tMismatch mother\tAnti-UPD\n";
+print "Chromosome\tTotal SNPs\tNon-informative\tMismatch father\tMismatch mother\tAnti-UPD\n";
 for my $chr (@chroms) {
 
     my $paternal = $sum{$chr}->{UPD_PATERNAL_ORIGIN};
