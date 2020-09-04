36e4aecd36949138c76d2f749d7140976a1419e2
Paul Piccinelli <paul.piccinelli@skane.se>
2020-09-04 12:04:36 +0200

    Added IQR to bin/qc_sentieon.pl

--- bin/qc_sentieon.pl
+++ bin/qc_sentieon.pl
@@ -42,13 +42,15 @@ if ($type eq "panel") {
 my $align_metrics_file = "aln_metrics.txt";
 my $insert_file = "is_metrics.txt";
 my $dedup_metrics_file = "dedup_metrics.txt";
-
 my %results;
 my $metrics_file;
+
 if ($type eq "wgs") {
     $metrics_file = "wgs_metrics.txt";
+    my ( $sum, %quartiles, $pct25_obs, $pct50_obs, $pct75_obs );
     open( HS, $metrics_file );
     while( <HS> ) {
+  
         if( /^\#SentieonCommandLine/ ) {
 	    <HS>;
             my $vals = <HS>;
@@ -56,6 +58,9 @@ if ($type eq "wgs") {
             $results{'median_cov'} = $a[3];
             $results{'sd_coverage'} = $a[2];
             $results{'mean_coverage'} = $a[1];
+            $pct25_obs = ( $a[0] + 1 ) / 4; # get observations from different quartiles
+            $pct50_obs = ( $a[0] + 1 ) / 2; # get observations from different quartiles
+            $pct75_obs = ( ( $a[0] + 1 ) * 3 ) / 4; # get observations from different quartiles
             $pct_above_x{'1'} = $a[12];
             $pct_above_x{'5'} = $a[13];
             $pct_above_x{'10'} = $a[14];
@@ -71,9 +76,21 @@ if ($type eq "wgs") {
             $pct_above_x{'90'} = $a[24];
             $pct_above_x{'100'} = $a[25];
 
-	    }
+	}elsif( /^(\d+)\s+(\d+)/ ){
+            $sum += $2;
+            if( $sum >= $pct25_obs and not $quartiles{ 'R_25' } ){
+                $quartiles{ 'R_25' } = $1;
+            }
+            if( $sum >= $pct50_obs and not $quartiles{ 'R_50' } ){
+                $quartiles{ 'R_50' } = $1;
+            }
+            if( $sum >= $pct75_obs and not $quartiles{ 'R_75' } ){
+                $quartiles{ 'R_75' } = $1;
+            }
+        }
     }
     close HS;
+    $results{'iqr'} = ( $quartiles{ 'R_75' } - $quartiles{ 'R_25' } );
 
 }
 elsif ($type eq "panel") {
