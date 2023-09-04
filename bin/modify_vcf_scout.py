cc276c3ca9112761c335d26687feefbf3578f74e
Jakob Willforss <jakob.willforss@hotmail.com>
2023-09-04 14:39:34 +0200

    First boiler

---
+++ bin/modify_vcf_scout.py
@@ -0,0 +1,21 @@
+import sys
+
+
+maxentscan = {
+    "placeholder": 1
+}
+
+clinmod = {
+    "placeholder": 1
+}
+
+rank = {
+    "placeholder": 1
+}
+
+# vep_csq?
+
+
+
+
+print(f"Hello world! First argument: {sys.argv[1]}")
---
+++ bin/python_modules/vcf.py
@@ -0,0 +1,31 @@
+print("hello world from module")
+
+def parse_vcf:
+    pass
+
+def parse_metainfo:
+    pass
+
+def parse_variant:
+    pass
+
+def parse_genotype:
+    pass
+
+def parse_info:
+    pass
+
+def parse_VEP_CSQ:
+    pass
+
+def remove_surrounding:
+    pass
+
+def keyval:
+    pass
+
+def excel_float:
+    pass
+
+def is_gzipped:
+    pass
