49347af808de373d19fcf16dbe62de39969a90f6
Viktor Henmyr <viktor_henmyr@hotmail.com>
2024-04-24 13:18:29 +0200

    99% vaf to homozygotes 1/1

--- bin/filter_mutect2_mito.pl
+++ bin/filter_mutect2_mito.pl
@@ -43,52 +43,56 @@ while ( my $a = $vcf->next_var() ) {
 
 sub genotype_quality {
     # Add placeholder 99 GQ to each individual
+    # Also update genotype to 1/1 if AF>99%
     my $gt = shift;
     my $c = 0;
     foreach my $ind ( @{ $gt }) {
        $gt->[$c]->{GQ} = 99;
+       if ($ind->{AF} >= 0.99) {
+            $gt->[$c]->{GT} = '1/1';
+       }
        $c++;
     }
     return $gt;
 }
 
 sub vcfstr {
-	my( $v, $sample_order ) = @_;
-	
-	my @all_info;
-	my $tot_str = $v->{CHROM}."\t".$v->{POS}."\t".$v->{ID}."\t".$v->{REF}."\t".$v->{ALT}."\t".$v->{QUAL}."\t".$v->{FILTER}."\t";
+    my( $v, $sample_order ) = @_;
+    
+    my @all_info;
+    my $tot_str = $v->{CHROM}."\t".$v->{POS}."\t".$v->{ID}."\t".$v->{REF}."\t".$v->{ALT}."\t".$v->{QUAL}."\t".$v->{FILTER}."\t";
 
-	# Generate and print INFO field
-	for my $info_key (@{$v->{INFO_order}}) {
-		if($info_key eq "CSQ") {
-			push @all_info, $info_key."=".$v->{_CSQstr};
-		}
-		else {
-			push @all_info, $info_key."=".$v->{INFO}->{$info_key};
-		}
-	}
-	$tot_str = $tot_str.join(";", @all_info)."\t";
+    # Generate and print INFO field
+    for my $info_key (@{$v->{INFO_order}}) {
+        if($info_key eq "CSQ") {
+            push @all_info, $info_key."=".$v->{_CSQstr};
+        }
+        else {
+            push @all_info, $info_key."=".$v->{INFO}->{$info_key};
+        }
+    }
+    $tot_str = $tot_str.join(";", @all_info)."\t";
 
-	# Print FORMAT field
-	$tot_str = $tot_str.join(":", @{$v->{FORMAT}})."\t";
+    # Print FORMAT field
+    $tot_str = $tot_str.join(":", @{$v->{FORMAT}})."\t";
 
 
-	my %order;
-	my $i=0;
-	if( @$sample_order > 0 ) {
-		$order{$_} = $i++ foreach @{$sample_order};
-	}
-	else {
-		$order{$_->{_sample_id}} = $i++ foreach @{$v->{GT}};
-	}
-	# Print GT fields for all samples
-	for my $gt ( sort {$order{$a->{_sample_id}} <=> $order{$b->{_sample_id}}} @{$v->{GT}}) {
-		my @all_gt;
-		for my $key ( @{$v->{FORMAT}} ) {
-			push @all_gt, ( defined $gt->{$key} ? $gt->{$key} : "");
-		}
-		$tot_str = $tot_str.join(":", @all_gt)."\t";
-	}
-	$tot_str = $tot_str."\n";
-	return $tot_str;
+    my %order;
+    my $i=0;
+    if( @$sample_order > 0 ) {
+        $order{$_} = $i++ foreach @{$sample_order};
+    }
+    else {
+        $order{$_->{_sample_id}} = $i++ foreach @{$v->{GT}};
+    }
+    # Print GT fields for all samples
+    for my $gt ( sort {$order{$a->{_sample_id}} <=> $order{$b->{_sample_id}}} @{$v->{GT}}) {
+        my @all_gt;
+        for my $key ( @{$v->{FORMAT}} ) {
+            push @all_gt, ( defined $gt->{$key} ? $gt->{$key} : "");
+        }
+        $tot_str = $tot_str.join(":", @all_gt)."\t";
+    }
+    $tot_str = $tot_str."\n";
+    return $tot_str;
 }
